aa699020c28842a75c4862031a2c119106e85cc5
BrianAvila-UNLA <brian.avilaxx03@gmail.com>
2026-08-27 16:43:55 -0300

    DAO y ABM de Festival
    Se completaron los .java de DAO y ABM de Festival.

--- src/negocio/FestivalABM.java
+++ src/negocio/FestivalABM.java
@@ -1,5 +1,43 @@
 package negocio;
 
-public class FestivalABM {
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
+import dao.FestivalDao;
+import datos.Festival;
+import datos.UnidadVenta;
+
+public class FestivalABM {
+	FestivalDao dao = new FestivalDao();
+
+	public Festival traer(long idFestival) {
+		return dao.traer(idFestival);
+	}
+
+	public int agregar(String nombre, String temporada, LocalDate fechaInicio, LocalDate fechaFin, Set<UnidadVenta> unidadesVenta) throws Exception {
+		//Excepcion por cliente duplicado
+		if(traer(nombre) != null)throw new Exception("ERROR ya existe festival con nombre: " + nombre);
+		Festival f = new Festival(nombre, temporada, fechaInicio, fechaFin, unidadesVenta);
+		return dao.agregar(f);
+	}
+
+	public void modificar(Festival f) {
+		dao.actualizar(f);
+	}
+
+	public void eliminar(long idFestival) {
+		/*
+		 * En este caso la baja es física y sabemos que la entidad no tiene relaciones
+		 * pero en caso de tenerlas, hay que validar que el cliente no tenga dependencias que generen errores al borrarlo.
+		 */
+		Festival f = dao.traer(idFestival);
+		// Implementar que si es null que arroje la excepción la Excepción de que el cliente no existe
+		dao.eliminar(f);
+	}
+
+	public List<Festival> traerPorTemporada(String temporada) {
+		return dao.traerPorTemporada(temporada);
+	}
 }
+
